dd82d4e4c35b75c49083f344f50323e3dee1e9dc
Miheer Patel <miheer2109@gmail.com>
2026-03-04 20:00:07 +0530

    removed extra stuff

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -10,8 +10,6 @@ import {
   User,
 } from "lucide-react";
 import { Link } from "react-router-dom";
-
-import AuthImagePattern from "../components/AuthImagePattern";
 import toast from "react-hot-toast";
 
 const SignUpPage = () => {
